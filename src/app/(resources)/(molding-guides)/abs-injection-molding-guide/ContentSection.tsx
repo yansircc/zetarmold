5e1b0fd45d6db41b797c97f58556e7b5756a6318
Frankie <93307889+frankie0736@users.noreply.github.com>
2025-02-21 00:40:30 +0800

    youtube url var name update

--- src/app/(resources)/(molding-guides)/abs-injection-molding-guide/ContentSection.tsx
+++ src/app/(resources)/(molding-guides)/abs-injection-molding-guide/ContentSection.tsx
@@ -64,7 +64,7 @@ export function ContentSection({ section, index }: Props) {
                   <DialogOverlay />
                   <DialogContent>
                     <video controls className="h-auto w-full">
-                      <source src={section.video.url} type="video/mp4" />
+                      <source src={section.video.youtubeUrl} type="video/mp4" />
                       Your browser does not support the video tag.
                     </video>
                   </DialogContent>
