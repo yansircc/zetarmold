7f8cde489ec087f4343d1a9dc57702bb05de10bd
Frankie <93307889+frankie0736@users.noreply.github.com>
2025-02-20 21:35:31 +0800

    add page: rapid prototyping

---
+++ src/app/(services)/rapid-prototyping/data.ts
@@ -0,0 +1,29 @@
+// Define the interface for video data
+export interface VideoData {
+  title: string;
+  youtubeUrl: string;
+}
+
+// Create an array of video data
+export const prototypeVideos: VideoData[] = [
+  {
+    title: 'How is the CNC prototype of injection molding products made?',
+    youtubeUrl: 'https://www.youtube.com/watch?v=ipYWIASMhHo',
+  },
+  {
+    title: 'What is low pressure injection molding and how does it work?',
+    youtubeUrl: 'https://www.youtube.com/watch?v=FLnSSZGQfco',
+  },
+  {
+    title: 'How SLA 3D Printing works？',
+    youtubeUrl: 'https://www.youtube.com/watch?v=DkLomBi08Ak',
+  },
+  {
+    title: 'How are prototypes made using a CNC machine',
+    youtubeUrl: 'https://www.youtube.com/watch?v=5RIqwSmqbNw',
+  },
+  {
+    title: 'How to 3D print a PLA prototype?',
+    youtubeUrl: 'https://www.youtube.com/watch?v=e9N_HmqU030',
+  },
+];
