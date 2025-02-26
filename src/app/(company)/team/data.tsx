54427232c0bf61efa5243781fc676dd74c4fdd2c
Frankie Xu <fx@FXMac.local>
2025-02-26 10:20:26 +0800

    feat: add page/team, update page/history

---
+++ src/app/(company)/team/data.tsx
@@ -0,0 +1,42 @@
+import type { GalleryItem } from '@/components/sections/gallery';
+
+export const PAGE_HEADER = {
+  title: 'Our Professional Team',
+  description:
+    'Zetar boasts a highly skilled and experienced team dedicated to delivering excellence in injection molding services. Our team includes specialized engineers, multilingual sales professionals, a meticulous QC team, and a loyal production workforce.',
+  action: {
+    text: 'Contact Us',
+    href: '/contact',
+  },
+};
+
+export const TEAM_ITEMS: GalleryItem[] = [
+  {
+    title: 'Engineer Team',
+    description:
+      'Zetar now has 8 engineers with more than 10 years of working experience in injection mold and industrial design',
+    image:
+      'https://zetarmold.com/wp-content/uploads/2024/04/%E5%9B%BE%E7%89%875.jpg',
+  },
+  {
+    title: 'Sales Team',
+    description:
+      'Zetar now has a 20+ sales team for overseas business with proficiency in English, Spanish, German, and Japanese languages',
+    image:
+      'https://zetarmold.com/wp-content/uploads/2024/06/Injection-Mold-Company-1.png',
+  },
+  {
+    title: 'QC Team',
+    description:
+      'The QC team has more than 10 staff members with rich experience and skills in quality checking and control processes',
+    image:
+      'https://zetarmold.com/wp-content/uploads/2024/04/%E5%9B%BE%E7%89%877-1-1.jpg',
+  },
+  {
+    title: 'Production Team',
+    description:
+      'Zetar has over 120 workers, 70% of whom have been with the company for more than 10 years, demonstrating stability, skill, knowledge, and loyalty',
+    image:
+      'https://zetarmold.com/wp-content/uploads/2024/04/%E5%9B%BE%E7%89%878.png',
+  },
+];
